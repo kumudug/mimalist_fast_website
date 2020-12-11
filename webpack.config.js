ac687d1db95c7a53f4a1d26d32e9cd8cc24f8f08
kumudug <kumudug1@gmail.com>
2020-12-11 15:03:46 -0800

    Load styles
    * `npm run build`
    * Open the html file.
    * View source. The style is added to the header
    * For production use a different config and [minimize](https://webpack.js.org/plugins/mini-css-extract-plugin/#minimizing-for-production)

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,15 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
+  module: {
+      rules: [
+          {
+              test: /\.css$/i,
+              use: ['style-loader', 'css-loader']
+          }
+      ]
+  }
 };
