07aa571fc4b8cd3bd51fb6bc41ac90caa4f1938e
kumudug <kumudug1@gmail.com>
2020-12-12 11:42:19 -0800

    Output Management
    * Setting different files into different bundles

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Notes from './data.csv';
 import toml from './data.toml';
 import yaml from './data.yaml';
 import json from './data.json5';
+import printMe from './print.js';
 
 console.log(toml.title); // output `TOML Example`
 console.log(toml.owner.name); // output `Tom Preston-Werner`
@@ -19,13 +20,21 @@ console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
 
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+  
+    element.appendChild(btn);
+
     // Add the image to our existing div.
     const myIcon = new Image();
+    myIcon.width = 15;
+    myIcon.height = 15;
     myIcon.src = Icon;
 
     element.appendChild(myIcon);
