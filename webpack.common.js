68e227f55b07f4b40eefa4a39f4b2cdbec842a6d
kumudug <kumudug1@gmail.com>
2020-12-12 18:50:19 -0800

    Added support for scss
    * Transpile, extract and include scss into the final html

--- webpack.common.js
+++ webpack.common.js
@@ -2,28 +2,51 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const options = {};
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
   devtool: 'inline-source-map',
   devServer: {
-   contentBase: './dist',
- },
- plugins: [
-   new CleanWebpackPlugin(),
-   new WebpackManifestPlugin(options),
-   new HtmlWebpackPlugin({
-     title: 'coding-mayhem',
-     template: 'src/index.html'
-   }),
- ],
+    contentBase: './dist',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new WebpackManifestPlugin(options),
+    new HtmlWebpackPlugin({
+      title: 'coding-mayhem',
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    })
+  ],
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-      rules: [
-      ]
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader' },
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer Dart Sass
+              implementation: require('sass'),
+
+              // See https://github.com/webpack-contrib/sass-loader/issues/804
+              webpackImporter: false,
+            },
+          },
+        ]
+      }
+    ]
   }
 };
