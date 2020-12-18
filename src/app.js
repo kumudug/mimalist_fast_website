7e1bbe3957422458392263c0da6633bd577bd95c
kumudu <kumudug1@gmail.com>
2020-12-17 21:09:51 -0800

    theming initial

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ import './app.scss';
 import {MDCTopAppBar} from '@material/top-app-bar';
 import {MDCRipple} from '@material/ripple';
 
+const selector = '.mdc-button, .mdc-card__primary-action';
+const gloRipples = [].map.call(document.querySelectorAll(selector), function(el) {
+  return new MDCRipple(el);
+});
+
 const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
 iconButtonRipple.unbounded = true;
 
